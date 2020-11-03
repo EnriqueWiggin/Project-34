e152a895ea5a586c1968b5e7043cebf82f389403
EnriqueWiggin <70081772+EnriqueWiggin@users.noreply.github.com>
2020-11-03 07:09:42 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,61 @@
+//Create variables here
+var dog
+var happyDog
+var database
+var foodS
+var foodStock
+function preload()
+{
+  happyDog = loadImage("images/dogImg.png");
+  dog2 = loadImage("images/dogImg1.png");
+}
+
+function setup() {
+  createCanvas(500, 500);
+  dog = createSprite(10,10);
+  dog.addImage(dog2);
+  
+  database=firebase.database();
+  foodStock=database.ref('Food');
+  foodStock.on("value",readStock);
+  
+}
+
+
+function draw() { 
+  background(46,139,87);
+  
+  
+  if(keyWentDown(UP_ARROW)) {
+    writeStock(foodS);
+    dog.addImage(happyDog);
+  }
+
+  drawSprites();
+  
+  textSize(15);
+  fill("black");
+  stroke("silver");
+  text("Press Up Arrow to Feed Dog Milk")
+
+}
+
+function readStock(data) {
+  foodS-data.val();
+}
+
+function writeStock(x) {
+
+  if(x<-0) {
+    x-0;
+  } else {
+    x=x-1;
+  }
+  
+database.ref('/').update({
+  Food:x
+  })
+}
+
+
+
